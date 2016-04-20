eccb1871d82cce3a283ba37a19067abcdc55bc68
OlafStaf <a.stafiyevsky@gmail.com>
2016-04-20 16:08:35 +0300

    implement navigation drawer layout support

--- app/src/main/java/com/stafiiyevskyi/mlsdev/droidfm/view/activity/MainActivity.java
+++ app/src/main/java/com/stafiiyevskyi/mlsdev/droidfm/view/activity/MainActivity.java
@@ -1,8 +1,11 @@
 package com.stafiiyevskyi.mlsdev.droidfm.view.activity;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,8 +14,34 @@ import android.view.MenuItem;
 
 import com.stafiiyevskyi.mlsdev.droidfm.R;
 
-public class MainActivity extends BaseActivity {
+import butterknife.Bind;
 
+public class MainActivity extends BaseActivity {
+    @Bind(R.id.drawer_layout)
+    DrawerLayout drNavigation;
+    @Bind(R.id.nav_view)
+    NavigationView nvNavigation;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        nvNavigation.setNavigationItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+                case R.id.albums_item:
+                    drNavigation.closeDrawers();
+                    return true;
+                case R.id.artists_item:
+                    drNavigation.closeDrawers();
+                    return true;
+                case R.id.charts_item:
+                    drNavigation.closeDrawers();
+                    return true;
+                default:
+                    return true;
+            }
+        });
+    }
 
     @Override
     public int getContentViewId() {
