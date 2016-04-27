6a0d5165f45c424920cfb53a6f8debe2945a0c39
OlafStaf <a.stafiyevsky@gmail.com>
2016-04-27 17:02:26 +0300

    created UI test using espresso (opening of drawer layout).

---
+++ app/src/androidTest/java/com/stafiiyevskyi/mlsdev/droidfm/MainActivityInstrumentationTest.java
@@ -0,0 +1,41 @@
+package com.stafiiyevskyi.mlsdev.droidfm;
+
+import android.support.design.widget.NavigationView;
+import android.support.test.rule.ActivityTestRule;
+import android.view.Gravity;
+
+import com.stafiiyevskyi.mlsdev.droidfm.view.activity.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.DrawerActions.open;
+import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
+import static android.support.test.espresso.core.deps.guava.base.Preconditions.checkNotNull;
+import static android.support.test.espresso.core.deps.guava.base.Preconditions.checkPositionIndex;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by oleksandr on 27.04.16.
+ */
+public class MainActivityInstrumentationTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+
+    @Test
+    public void testOpenNavigationDrawer() {
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT)))
+                .perform(open());
+
+        NavigationView navigationView = (NavigationView) activityTestRule.getActivity().findViewById(R.id.nav_view);
+        checkNotNull(navigationView, "navigationView is null");
+        final int count = navigationView.getMenu().size();
+        checkPositionIndex(0, count, "No 1 index " + count + " size");
+    }
+}
